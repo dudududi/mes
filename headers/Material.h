e1650d90332c92af2173d960a9675ae5ded7b0ec
dudududi <kamil.dudek@gmail.com>
2015-12-06 18:29:49 +0100

    material changed from class to struct

--- headers/Material.h
+++ headers/Material.h
@@ -6,8 +6,7 @@
 #define MES_MATERIAL_H
 
 
-class Material {
-public:
+struct Material {
     int nh, ne;
     double L, K, S;
     static const int NUMBER_OF_VARS = 5;
--- sources/Material.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Kamil on 06.12.15.
-//
-
-#include "../headers/Material.h"
